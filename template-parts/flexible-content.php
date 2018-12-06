4c5527511dfb330e1a835fc643678d50bb9b20e7
Unknown <repac1987@gmail.com>
2018-12-06 10:53:57 +0100

    Fixed slider group

--- template-parts/flexible-content.php
+++ template-parts/flexible-content.php
@@ -1,5 +1,7 @@
 <?php if ( have_rows( 'flexible_content' ) ): ?>
+	<?php $count = 0; ?>
 	<?php while ( have_rows( 'flexible_content' ) ) : the_row(); ?>
+		<?php $count++; ?>
 
 		<!-- Block Section -->
 		<?php if ( get_row_layout() == 'block_section' ) : ?>
@@ -38,7 +40,7 @@
 			                <ul class="slides">
 							<?php foreach ( $picture_slider_images as $picture_slider_image ): ?>
 			                    <li>
-			                    	<a href="<?php echo $picture_slider_image['sizes']['large']; ?>" rel="group">
+			                    	<a href="<?php echo $picture_slider_image['sizes']['large']; ?>" rel="group-<?php echo $count; ?>">
 									<img src="<?php echo $picture_slider_image['sizes']['slider_thumb']; ?>" alt="<?php echo $picture_slider_image['alt']; ?>" />
 									</a>
 			                    </li>
@@ -50,7 +52,7 @@
 								<span class="zero2"></span><span class="total-slides"></span>
 							</div>
 			            </div>
-						<?php endif; ?>
+					<?php endif; ?>
 					<?php elseif ($block_type === 'image-gallery'): ?>
 						<?php $image_gallery_images = get_sub_field( 'image_gallery' ); ?>
 						<?php if ( $image_gallery_images ) :  ?>
@@ -106,7 +108,7 @@
 			                <ul class="slides">
 							<?php foreach ( $picture_slider_images as $picture_slider_image ): ?>
 			                    <li>
-			                    	<a href="<?php echo $picture_slider_image['sizes']['large']; ?>" rel="group">
+			                    	<a href="<?php echo $picture_slider_image['sizes']['large']; ?>" rel="group-<?php echo $count; ?>">
 									<img src="<?php echo $picture_slider_image['sizes']['slider_thumb']; ?>" alt="<?php echo $picture_slider_image['alt']; ?>"/>
 									</a>
 			                    </li>
